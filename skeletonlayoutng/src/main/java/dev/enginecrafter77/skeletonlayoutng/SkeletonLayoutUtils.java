b532f91836f87b3573b41f9538fe1ff679ad10c7
Enginecrafter77 <hutiramichal@gmail.com>
2023-10-18 23:15:27 +0200

    refactor: Isolated SkeletonOverlay

--- skeletonlayoutng/src/main/java/dev/enginecrafter77/skeletonlayoutng/SkeletonLayoutUtils.java
+++ skeletonlayoutng/src/main/java/dev/enginecrafter77/skeletonlayoutng/SkeletonLayoutUtils.java
@@ -20,7 +20,6 @@ import android.view.View;
 
 import androidx.annotation.IdRes;
 import androidx.annotation.LayoutRes;
-import androidx.annotation.UiThread;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class SkeletonLayoutUtils {
@@ -37,38 +36,5 @@ public class SkeletonLayoutUtils {
 		SkeletonAdapter adapter = new SkeletonAdapter(itemLayout, itemSkeletons);
 		adapter.setItemCount(count);
 		return new RecyclerViewSkeleton(recyclerView, adapter);
-	}
-
-	private static class SkeletonOverlay extends SkeletonDrawable implements DetachableSkeleton
-	{
-		private final View.OnLayoutChangeListener onLayoutChangeListener;
-
-		private final View view;
-
-		public SkeletonOverlay(View view)
-		{
-			super();
-			this.view = view;
-			this.onLayoutChangeListener = this::onViewLayoutChange;
-		}
-
-		protected void install()
-		{
-			this.view.getOverlay().add(this);
-			this.view.addOnLayoutChangeListener(this.onLayoutChangeListener);
-		}
-
-		@UiThread
-		protected void onViewLayoutChange(View view, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom)
-		{
-			this.setBounds(left, top, right, bottom);
-		}
-
-		@Override
-		public void detachSkeleton()
-		{
-			this.view.getOverlay().remove(this);
-			this.view.removeOnLayoutChangeListener(this.onLayoutChangeListener);
-		}
 	}
 }
---
+++ skeletonlayoutng/src/main/java/dev/enginecrafter77/skeletonlayoutng/SkeletonOverlay.java
@@ -0,0 +1,37 @@
+package dev.enginecrafter77.skeletonlayoutng;
+
+import android.view.View;
+
+import androidx.annotation.UiThread;
+
+class SkeletonOverlay extends SkeletonDrawable implements DetachableSkeleton {
+	private final View.OnLayoutChangeListener onLayoutChangeListener;
+
+	private final View view;
+
+	public SkeletonOverlay(View view)
+	{
+		super();
+		this.view = view;
+		this.onLayoutChangeListener = this::onViewLayoutChange;
+	}
+
+	protected void install()
+	{
+		this.view.getOverlay().add(this);
+		this.view.addOnLayoutChangeListener(this.onLayoutChangeListener);
+	}
+
+	@UiThread
+	protected void onViewLayoutChange(View view, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom)
+	{
+		this.setBounds(left, top, right, bottom);
+	}
+
+	@Override
+	public void detachSkeleton()
+	{
+		this.view.getOverlay().remove(this);
+		this.view.removeOnLayoutChangeListener(this.onLayoutChangeListener);
+	}
+}
